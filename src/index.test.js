2de35b44148023307c6e054c4e3f2d4eaabcec2f
Kaio Silveira <silveira.kaio@icloud.com>
2023-08-30 16:22:56 +0100

    setup example + unit tests + CI

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,17 @@
-import { toBeRefactored } from './index';
+import { Order } from '.';
 
-describe('functionToBeRefactored', () => {
-  it('should work', () => {
-    expect(toBeRefactored()).toEqual('Hello, world!');
+describe('Order', () => {
+  describe('price', () => {
+    it('should apply a standard discount of 2% for orders with item quantities below 1000', () => {
+      const item = { price: 100 };
+      const order = new Order(10, item);
+      expect(order.price).toBe(980);
+    });
+
+    it('should apply an additional discount of 3% for orders with item quantities above 1000', () => {
+      const item = { price: 100 };
+      const order = new Order(11, item);
+      expect(order.price).toBe(1045);
+    });
   });
 });
