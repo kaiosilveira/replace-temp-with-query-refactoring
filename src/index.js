d1deb868fc41678543b9668b4f592244cd32c87f
Kaio Silveira <silveira.kaio@icloud.com>
2023-08-30 16:26:44 +0100

    extract discountFactor getter

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ export class Order {
   }
 
   get price() {
-    var discountFactor = 0.98;
-
-    if (this.basePrice > 1000) discountFactor -= 0.03;
-
+    var discountFactor = this.discountFactor;
     return this.basePrice * discountFactor;
   }
 
   get basePrice() {
     return this._quantity * this._item.price;
+  }
+
+  get discountFactor() {
+    let discountFactor = 0.98;
+    if (this.basePrice > 1000) discountFactor -= 0.03;
+    return discountFactor;
   }
 }
